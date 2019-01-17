700ab69281b084fe631da0c96203cfbb0a9ea07c
Riccardo Amadio aka rokity <riccardo.amadio1@gmail.com>
2019-01-17 18:01:55 +0100

    update certifications

--- src/static/data/certification.js
+++ src/static/data/certification.js
@@ -1,5 +1,8 @@
  const certification = [
     {
         "value":"Course of TDD and Unit Testing with Javascript from DevMarche Community"
+    },
+    {
+        "value":"Google Developer Challenge Scholarship 2017"
     }
 ]
