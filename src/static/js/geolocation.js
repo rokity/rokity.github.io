b3517d542bb8385b7c402e319a2eb5e949cd15fa
Riccardo Amadio aka rokity <riccardo.amadio1@gmail.com>
2019-01-20 19:29:36 +0100

    firebase enabled and developed

--- src/static/js/geolocation.js
+++ src/static/js/geolocation.js
@@ -1,4 +1,24 @@
-
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyB7IA52cFpYfyQ4hYc3CU_XsFRwgkMHaCg",
+    authDomain: "visitors-aeb4b.firebaseapp.com",
+    databaseURL: "https://visitors-aeb4b.firebaseio.com",
+    projectId: "visitors-aeb4b",
+    storageBucket: "visitors-aeb4b.appspot.com",
+    messagingSenderId: "37669232163"
+  };
+ firebase.initializeApp(config);
+ var db = firebase.firestore();
+  //Write User Data
+  function writeUserData(json) {
+    db.collection("visitatori").add(json)
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+  }
 function geolocation() {
     fetch('https://ipapi.co/json')
         .then(function (response) { return response.text(); })
@@ -7,13 +27,8 @@ function geolocation() {
         })
         .then(function(json){
             console.log("aa")
-            fetch("https://test-hobby.herokuapp.com/",
-            {
-                method: "POST",
-                body: JSON.stringify(json)
-            })
-            .then(function(res){ return })
-            .then(function(data){ return })
+            return writeUserData(json)
         })
         .catch(function (error) { console.error(error);return });
 }
+
