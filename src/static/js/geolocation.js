e2ba6f824dcda0489dca10d32f17e34611eefb16
Riccardo Amadio aka rokity <riccardo.amadio1@gmail.com>
2019-01-22 16:27:06 +0100

    firebase production mode

--- src/static/js/geolocation.js
+++ src/static/js/geolocation.js
@@ -1,35 +1,40 @@
-  // Initialize Firebase
-  const config = {
-    apiKey: "AIzaSyB7IA52cFpYfyQ4hYc3CU_XsFRwgkMHaCg",
-    authDomain: "visitors-aeb4b.firebaseapp.com",
-    databaseURL: "https://visitors-aeb4b.firebaseio.com",
-    projectId: "visitors-aeb4b",
-    storageBucket: "visitors-aeb4b.appspot.com",
-    messagingSenderId: "37669232163"
-  };
- firebase.initializeApp(config);
- var db = firebase.firestore();
-  //Write User Data
-  function writeUserData(json) {
+//Write User Data
+function writeUserData(json) {
     db.collection("visitatori").add(json)
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
-  }
+        .then(function (docRef) {
+            return
+        })
+        .catch(function (error) {
+            // console.error("Error adding document: ", error);
+            return
+        });
+}
 function geolocation() {
     fetch('https://ipapi.co/json')
         .then(function (response) { return response.text(); })
         .then(function (text) {
             return JSON.parse(text)
         })
-        .then(function(json){
-            json['date']= new Date();
-            console.log(json)
+        .then(function (json) {
+            json['date'] = new Date();
             return writeUserData(json)
         })
-        .catch(function (error) { console.error(error);return });
+        .catch(function (error) { 
+            // console.error(error); 
+            return });
 }
 
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyB7IA52cFpYfyQ4hYc3CU_XsFRwgkMHaCg",
+    authDomain: "visitors-aeb4b.firebaseapp.com",
+    databaseURL: "https://visitors-aeb4b.firebaseio.com",
+    projectId: "visitors-aeb4b",
+    storageBucket: "visitors-aeb4b.appspot.com",
+    messagingSenderId: "37669232163"
+};
+firebase.initializeApp(config);
+var db = firebase.firestore();
+const settings = { timestampsInSnapshots: true };
+db.settings(settings);
