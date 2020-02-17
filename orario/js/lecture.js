a5ab5826c87da6c5a85b2350c94977340a433c12
Riccardo Amadio <riccardo.amadio1@gmail.com>
2020-02-17 09:12:07 +0100

    lectures mobile work but there are bugs

--- orario/js/lecture.js
+++ orario/js/lecture.js
@@ -1,30 +1,37 @@
-if(isMobile())
-{
-    document.getElementById("orario").setAttribute("style","display:none")
-    // let today = new Date();    
-    let today = new Date(2020, 2, 16,18,30);
-    if(isWeekend(today))
-    {
-        let h1=document.createElement("h1");
-        h1.innerHTML+="It's Weekend!! </br> It's STUDY Time !!"
-        h1.className+="weekend"
+if (isMobile()) {
+    document.getElementById("orario").setAttribute("style", "display:none")
+    let today = new Date();
+    // let today = new Date(2020, 2, 16,18,30);
+    if (isWeekend(today)) {
+        let h1 = document.createElement("h1");
+        h1.innerHTML += "It's Weekend!! </br> It's STUDY Time !!"
+        h1.className += "weekend"
         document.body.appendChild(h1)
     }
-    else if(today.getHours()<9 || today.getHours()>=18)
-    {
-        let h1=document.createElement("h1");
-        h1.innerHTML+="I don't have lectures now!! </br> It's STUDY Time !!"
-        h1.className+="weekend"
+    else if (today.getHours() < 9 || today.getHours() >= 18) {
+        let h1 = document.createElement("h1");
+        h1.innerHTML += "I don't have lectures now!! </br> It's STUDY Time !!"
+        h1.className += "weekend"
         document.body.appendChild(h1)
     }
-    else{
+    else {
         let lectures = orario[getDay(today).toLowerCase()]
-        let hour=today.getHours()
-        let keys=Object.keys(lectures)        
-        let exact_hour=`${hour}:${(hour+2)}`
-        let one_more=`${hour+1}:${(hour+3)}`
-        let less_one=`${hour-1}:${hour+1}`
-
+        let hour = today.getHours()
+        let keys = Object.keys(lectures)
+        let exact_hour = `${hour}:${(hour + 2)}`
+        let one_more = `${hour + 1}:${(hour + 3)}`
+        let less_one = `${hour - 1}:${hour + 1}`
+        let index = -1;
+        if (keys.indexOf(exact_hour) != -1)
+            index = keys.indexOf(exact_hour)
+        if (keys.indexOf(one_more) != -1)
+            index = keys.indexOf(one_more)
+        if (keys.indexOf(less_one) != -1)
+            index = keys.indexOf(less_one)
+        let h1 = document.createElement("h1");
+        h1.innerHTML += lectures[exact_hour]["long_name"] + "</br>"+ lectures[exact_hour]["room"]
+        h1.className += "weekend"
+        document.body.appendChild(h1)
     }
 }
 
